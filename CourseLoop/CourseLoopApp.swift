1d2c2d77c6997267c5fcce89adf5bca087042a7f
akash patel <akashp1412@mail.com>
2025-04-19 10:56:00 -0400

    Initial Commit

---
+++ CourseLoop/CourseLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseLoopApp.swift
+//  CourseLoop
+//
+//  Created by Akash Patel on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CourseLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CourseLoopTests/CourseLoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourseLoopTests.swift
+//  CourseLoopTests
+//
+//  Created by Akash Patel on 4/19/25.
+//
+
+import Testing
+@testable import CourseLoop
+
+struct CourseLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
